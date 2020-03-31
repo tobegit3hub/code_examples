a4b982401b8adf7624f76660a3942501e9001f8b
tobe <tobeg3oogle@gmail.com>
2020-03-31 15:23:34 +0800

    Add antlr sql g4 and implementation of visitor

---
+++ antlr_example/src/main/java/com/tobe/sql/Main.java
@@ -0,0 +1,18 @@
+package com.tobe.sql;
+
+import com.tobe.sql.*;
+import org.antlr.v4.runtime.*;
+
+public class Main {
+
+    public static void main(String[] argv) {
+        String sqlText = "SELECT C1, C2 FROM T1";
+
+        MySqlVisitor visitor = new MySqlVisitor();
+        SqlBaseLexer lexer = new SqlBaseLexer(CharStreams.fromString(sqlText));
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        SqlBaseParser parser = new SqlBaseParser(tokenStream);
+        parser.singleStatement().accept(visitor);
+    }
+
+}
---
+++ antlr_example/src/main/java/com/tobe/sql/MySqlVisitor.java
@@ -0,0 +1,33 @@
+package com.tobe.sql;
+
+import com.tobe.sql.SqlBaseBaseVisitor;
+import com.tobe.sql.SqlBaseParser.*;
+
+public class MySqlVisitor extends SqlBaseBaseVisitor {
+
+    @Override
+    public String visitFromClause(SqlBaseParser.FromClauseContext ctx) {
+        String tableName = visitRelation(ctx.relation(0));
+        System.out.println("SQL table name: " + tableName);
+        return tableName;
+    }
+
+    @Override
+    public String visitRelation(SqlBaseParser.RelationContext ctx) {
+        if (ctx.relationPrimary() instanceof TableNameContext) {
+            return visitTableName((TableNameContext)ctx.relationPrimary());
+        }
+        return "";
+    }
+
+    @Override
+    public String visitTableName(SqlBaseParser.TableNameContext ctx) {
+        return visitTableIdentifier(ctx.tableIdentifier());
+    }
+
+    @Override
+    public String visitTableIdentifier(SqlBaseParser.TableIdentifierContext ctx) {
+        return ctx.getChild(0).getText();
+    }
+
+}
